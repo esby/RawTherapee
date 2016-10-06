d036491dbfa9655d85e0b1f186e3f40855e558c9
esby <esby@free.fr>
2016-10-06 21:49:30 +0200

    C++ static_cast style

--- rtgui/ttfavoritecolorer.cc
+++ rtgui/ttfavoritecolorer.cc
@@ -185,7 +185,7 @@ void TTFavoriteColorChooser::themeImport(std::ifstream& myfile)
   while (condition)
   {
     int position = myfile.tellg();
-    condition = (bool) getline (myfile,line);
+    condition = static_cast<bool> getline (myfile,line);
 
     std::istringstream tokensplitter(line);
     std::string token;
