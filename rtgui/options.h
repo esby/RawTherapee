336871e473eb4f06b5b26b60a92a3fa77d7ba9a8
esby <esby@free.fr>
2016-10-25 11:10:52 +0200

    cosmetic: unused parameter removal

--- rtgui/options.h
+++ rtgui/options.h
@@ -309,7 +309,6 @@ public:
     Glib::ustring lastVibranceCurvesDir;
     Glib::ustring lastProfilingReferenceDir;
     Glib::ustring lastBWCurvesDir;
-    Glib::ustring lastTTSaverDir;
 
     size_t maxRecentFolders;                   // max. number of recent folders stored in options file
     std::vector<Glib::ustring> recentFolders;  // List containing all recent folders
