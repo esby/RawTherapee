0ea5e76358c1691ca59653a8c7ec0d45fcb3dd18
keby <keby@lorien.univ-tln.fr>
2018-01-04 01:43:58 +0100

    adding variables to pass some interesting values (eg: iso values) to ttfilter)

---
+++ rtgui/ttvardisplayer.cc
@@ -0,0 +1,105 @@
+/*
+ *  This file is part of RawTherapee.
+ *
+ *  Copyright (c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
+ *
+ *  RawTherapee is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  RawTherapee is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RawTherapee.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "ttvardisplayer.h"
+#include "options.h"
+#include "guiutils.h"
+#include "rtimage.h"
+#include "rtdef.h"
+#include "guiutils.h"
+#include <fstream>
+
+using namespace rtengine;
+using namespace rtengine::procparams;
+
+TTVarDisplayer::TTVarDisplayer () : FoldableToolPanel(this,"ttvardisplayer",M("TT_VAR_DISPLAYER_LABEL"),false,false)
+{
+        //todo: find a f!cking way to set a minimum size for a tool
+	hboxr = Gtk::manage(new Gtk::HBox());
+	pack_start(*hboxr, Gtk::PACK_SHRINK, 0);
+
+	vbox1 = Gtk::manage(new Gtk::VBox());
+	vbox1->set_spacing(4);
+        Gtk::Label* labelVar = Gtk::manage(new Gtk::Label (M("TT_VAR_DISPLAYER_LABEL_VARIABLES")));
+        vbox1->pack_start(*labelVar, Gtk::PACK_SHRINK, 0);
+
+	hboxr->pack_start(*vbox1, Gtk::PACK_EXPAND_WIDGET ,0);
+}
+
+void TTVarDisplayer::deploy()
+{
+	FoldableToolPanel::deploy();
+}
+
+void TTVarDisplayer::deployLate()
+{
+	FoldableToolPanel::deployLate();
+}
+
+void TTVarDisplayer::react(FakeProcEvent ev)
+{
+	if (ev == FakeEvExifTransmitted)
+	{
+		// we first create labels & entries to display the env variables
+		for (size_t i=varBox.size(); i<env->countVar(); i++)
+		{
+			Gtk::HBox* hbox = Gtk::manage(new Gtk::HBox());
+			hbox->set_spacing(4);
+
+			Gtk::Label* lbl = Gtk::manage(new Gtk::Label ());
+			lbl->set_width_chars(6);
+			hbox->pack_start(*lbl,  Gtk::PACK_SHRINK,0);
+
+			Gtk::Entry* entry = Gtk::manage (new Gtk::Entry ());
+			entry->set_width_chars(25);
+			hbox->pack_start(*entry, Gtk::PACK_SHRINK,0);
+
+			vbox1->pack_start(*hbox, Gtk::PACK_SHRINK,0);
+
+			varBox.push_back(hbox);
+			varLabel.push_back(lbl);
+			varEntry.push_back(entry);
+		}
+
+		// we feed the new values
+		printf("TTVarDisplayer React \n");
+		for (size_t i=0; i<env->countVar(); i++)
+		{
+			RtVariable* d = env->getVariable(i);
+			if (d != nullptr)
+			{
+				varLabel[i]->set_text(d->getName());
+				varEntry[i]->set_text(d->toString());
+				printf("variable : %s ", d->getName().c_str());
+				printf("value: %s \n", d->toString().c_str());
+			}
+		}
+
+
+		// we display the entries
+		getExpander()->show_all();
+
+	}
+}
+
+//void TTUDLRHider::on_toggle_button()
+void TTVarDisplayer::enabledChanged  () 
+{
+   
+}
+
---
+++ rtgui/variable.cc
@@ -0,0 +1,197 @@
+/*
+ *  This file is part of RawTherapee.
+ *
+ *  Copyright (c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
+ *
+ *  RawTherapee is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  RawTherapee is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RawTherapee.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "variable.h"
+
+using namespace rtengine::procparams;
+
+
+// RtVariableStore dummy implementation
+int RtVariableStore::getAsInt() {}
+double RtVariableStore::getAsDouble(){}
+Glib::ustring RtVariableStore::getAsString(){}
+bool RtVariableStore::getAsBool(){}
+
+void RtVariableStore::setAsInt(int v){}
+void RtVariableStore::setAsDouble(double v){}
+void RtVariableStore::setAsString(Glib::ustring v){}
+void RtVariableStore::setAsBool(bool v){}
+
+
+// class RtVariableIntStore 
+int RtVariableIntStore::getAsInt()
+{
+  return value;
+}
+
+void RtVariableIntStore::setAsInt(int v)
+{
+  value = v;
+}
+// class RtVariableDoubleStore
+double RtVariableDoubleStore::getAsDouble()
+{
+  return value;
+}
+
+void RtVariableDoubleStore::setAsDouble(double v)
+{
+  value = v;
+}
+
+// class RtVariableStringStore
+Glib::ustring RtVariableStringStore::getAsString()
+{
+  return value;
+}
+
+void RtVariableStringStore::setAsString(Glib::ustring v)
+{
+  //todo: should the string be copied ?
+  value = v;
+}
+
+// class RtVariableBoolStore 
+bool RtVariableBoolStore::getAsBool()
+{
+  return value;
+}
+
+void RtVariableBoolStore::setAsBool(bool v)
+{
+  value = v;
+}
+
+
+// class RtVariable
+RtVariable::RtVariable(Glib::ustring _name, Environment* _env)
+{
+  type = RT_VARIABLE_TYPE_UNDEF;
+  value = nullptr;
+  name = _name;
+  env = _env;
+}
+Environment* RtVariable::getEnv() 
+{
+  return env;
+}
+
+void RtVariable::setName(Glib::ustring _name) 
+{
+  name = _name; 
+}
+Glib::ustring RtVariable::getName() 
+{
+  return name;
+}
+
+int RtVariable::getAsInt() 
+{ 
+  if (type == RT_VARIABLE_TYPE_INT ) 
+    return static_cast<RtVariableIntStore*>(value)->getAsInt(); 
+  return 0;
+}
+
+double RtVariable::getAsDouble() 
+{  
+  if (type == RT_VARIABLE_TYPE_DOUBLE ) 
+    return static_cast<RtVariableDoubleStore*>(value)->getAsDouble() ; 
+  return 0;
+}
+
+Glib::ustring RtVariable::getAsString() 
+{  
+  if (type == RT_VARIABLE_TYPE_STRING ) 
+    return static_cast<RtVariableStringStore*>(value)->getAsString() ; 
+  return "";
+}
+
+bool RtVariable::getAsBool() 
+{  
+  if (type == RT_VARIABLE_TYPE_BOOL ) 
+    return static_cast<RtVariableBoolStore*>(value)->getAsBool() ; 
+  return false;
+}
+
+int RtVariable::getType() 
+{ 
+  return type;
+}
+
+void RtVariable::setAsInt(int v)
+{
+  if (type != RT_VARIABLE_TYPE_INT)
+  {
+if (value!= nullptr) delete value;
+value = new RtVariableIntStore();
+type = RT_VARIABLE_TYPE_INT;
+  }
+  value->setAsInt(v);
+}
+
+void RtVariable::setAsDouble(double v)
+{
+  if (type != RT_VARIABLE_TYPE_DOUBLE)
+  {
+if (value!= nullptr) delete value;
+value = new RtVariableDoubleStore();
+type = RT_VARIABLE_TYPE_DOUBLE;
+  }
+  value->setAsDouble(v);
+}
+
+void RtVariable::setAsString(Glib::ustring v)
+{
+  if (type != RT_VARIABLE_TYPE_STRING)
+  {
+if (value!= nullptr) delete value;
+value = new RtVariableStringStore();
+type = RT_VARIABLE_TYPE_STRING;
+  }
+  value->setAsString(v);
+}
+
+
+void RtVariable::setAsBool(bool v)
+{
+  if (type != RT_VARIABLE_TYPE_BOOL)
+  {
+if (value!= nullptr) delete value;
+value = new RtVariableBoolStore();
+type = RT_VARIABLE_TYPE_BOOL;
+ }
+  value->setAsBool(v);
+   }
+
+Glib::ustring RtVariable::toString() 
+{
+  if (type == RT_VARIABLE_TYPE_STRING) return getAsString();
+  if (type == RT_VARIABLE_TYPE_INT)
+  {  
+ return g_strdup_printf("%i", getAsInt());
+  }
+
+  if (type == RT_VARIABLE_TYPE_DOUBLE)
+  {
+ return g_strdup_printf("%f", getAsDouble());
+  }
+
+  if (type == RT_VARIABLE_TYPE_BOOL)
+ return getAsBool() ? "true" : "false";
+}
+
